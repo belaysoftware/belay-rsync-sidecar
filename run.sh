4d0029d9ccd9916a606548ac5f5723482f4a2de6
Nick Nystrom <nnystrom@gmail.com>
2022-08-20 13:56:42 -0500

    Pull before each push

--- run.sh
+++ run.sh
@@ -3,20 +3,19 @@ set -euo pipefail
 
 echo "Configured to sync from ${SOURCE_FOLDER} to ${TARGET_GIT_URL}"
 TARGET_FOLDER=/tmp/target
-if [ -d ${TARGET_FOLDER} ]
+if [ ! -d ${TARGET_FOLDER} ]
 then
-    cd ${TARGET_FOLDER}
-    git pull
-else
     git clone ${TARGET_GIT_URL} ${TARGET_FOLDER}
-    cd ${TARGET_FOLDER}
 fi
+cd ${TARGET_FOLDER}
 
 sync() {
     rsync -av --chown root:root --delete --exclude=.git ${SOURCE_FOLDER}/ ${TARGET_FOLDER}
     if [ $(git status --porcelain | wc -l) -eq "0" ]; then
         echo "No changes to commit"
     else
+        echo "Updating local target git repo"
+        git pull
         echo "Syncing changes to ${TARGET_GIT_URL}"
         git add .
         git commit -m "Sync from ${HOSTNAME}"
